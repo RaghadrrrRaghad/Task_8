f3e1555988afabdcbcae98d2d57d16b0f68c8574
Raghad <rghdhnya@gmail.com>
2023-06-21 14:59:08 +0300

    objects of  Student class

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ s2 = subject_module.Subject("English")
 s3 = subject_module.Subject("Arabic")
 
 
+st1 = student_module.Student("Amal", 18, "Gaza")
+st2 = student_module.Student("Ahlam", 18, "Rafah")
+st3 = student_module.Student("Ali", 19, "Gaza")
+
+
